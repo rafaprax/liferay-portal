496702887989021eee8f4023bfcc1a100ed30839
Leonardo Barros <leonardo.barros@liferay.com>
2015-07-20 16:36:30 -0300

    LPS-57255 Move search package

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/com/liferay/dynamic/data/mapping/search/StructureDisplayTerms.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/com/liferay/dynamic/data/mapping/search/StructureDisplayTerms.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.dynamicdatamapping.search;
+package com.liferay.dynamic.data.mapping.search;
 
 import com.liferay.portal.kernel.dao.search.DisplayTerms;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -22,20 +22,27 @@ import javax.portlet.PortletRequest;
 /**
  * @author Eduardo Lundgren
  */
-public class TemplateDisplayTerms extends DisplayTerms {
+public class StructureDisplayTerms extends DisplayTerms {
+
+	public static final String CLASS_NAME_ID = "classNameId";
 
 	public static final String DESCRIPTION = "description";
 
 	public static final String NAME = "name";
 
-	public static final String TYPE = "type";
+	public static final String STORAGE_TYPE = "storageType";
 
-	public TemplateDisplayTerms(PortletRequest portletRequest) {
+	public StructureDisplayTerms(PortletRequest portletRequest) {
 		super(portletRequest);
 
+		classNameId = ParamUtil.getLong(portletRequest, CLASS_NAME_ID);
 		description = ParamUtil.getString(portletRequest, DESCRIPTION);
 		name = ParamUtil.getString(portletRequest, NAME);
-		type = ParamUtil.getString(portletRequest, TYPE);
+		storageType = ParamUtil.getString(portletRequest, STORAGE_TYPE);
+	}
+
+	public long getClassNameId() {
+		return classNameId;
 	}
 
 	public String getDescription() {
@@ -46,12 +53,13 @@ public class TemplateDisplayTerms extends DisplayTerms {
 		return name;
 	}
 
-	public String getType() {
-		return type;
+	public String getStorageType() {
+		return storageType;
 	}
 
+	protected long classNameId;
 	protected String description;
 	protected String name;
-	protected String type;
+	protected String storageType;
 
 }
