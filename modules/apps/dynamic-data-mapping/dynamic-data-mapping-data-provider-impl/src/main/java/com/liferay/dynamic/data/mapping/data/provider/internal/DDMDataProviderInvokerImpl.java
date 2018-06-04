eb7fa923c68e2affdd309b8c6d156ca7c2faca77
Leonardo Barros <leonardo.barros@liferay.com>
2018-06-04 13:11:17 -0300

    LPS-81563 Apply the changes to API

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-impl/src/main/java/com/liferay/dynamic/data/mapping/data/provider/internal/DDMDataProviderInvokerImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-impl/src/main/java/com/liferay/dynamic/data/mapping/data/provider/internal/DDMDataProviderInvokerImpl.java
@@ -15,19 +15,13 @@
 package com.liferay.dynamic.data.mapping.data.provider.internal;
 
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
-import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderContext;
-import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderContextContributor;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderInvoker;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderRequest;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderResponse;
-import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderResponse.Status;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderResponseStatus;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderTracker;
-import com.liferay.dynamic.data.mapping.io.DDMFormValuesJSONDeserializer;
 import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
-import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceService;
-import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
-import com.liferay.dynamic.data.mapping.util.DDMFormFactory;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -37,8 +31,6 @@ import com.liferay.portal.kernel.util.Validator;
 import com.netflix.hystrix.exception.HystrixRuntimeException;
 import com.netflix.hystrix.exception.HystrixRuntimeException.FailureType;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.osgi.service.component.annotations.Component;
@@ -60,7 +52,7 @@ public class DDMDataProviderInvokerImpl implements DDMDataProviderInvoker {
 			if (_log.isWarnEnabled()) {
 				_log.warn(
 					"Unable to invoke DDM Data Provider instance ID " +
-						ddmDataProviderRequest.getDDMDataProviderInstanceId(),
+						ddmDataProviderRequest.getDDMDataProviderId(),
 					e);
 			}
 
@@ -68,90 +60,48 @@ public class DDMDataProviderInvokerImpl implements DDMDataProviderInvoker {
 		}
 	}
 
-	protected void addDDMDataProviderRequestParameters(
-		DDMDataProviderRequest ddmDataProviderRequest,
-		DDMDataProviderInstance ddmDataProviderInstance) {
-
-		// Context Contributors
-
-		List<DDMDataProviderContextContributor>
-			ddmDataProviderContextContributors =
-				ddmDataProviderTracker.getDDMDataProviderContextContributors(
-					ddmDataProviderInstance.getType());
-
-		for (DDMDataProviderContextContributor
-				ddmDataProviderContextContributor :
-					ddmDataProviderContextContributors) {
-
-			Map<String, String> parameters =
-				ddmDataProviderContextContributor.getParameters(
-					ddmDataProviderRequest.getHttpServletRequest());
-
-			if (parameters == null) {
-				continue;
-			}
-
-			ddmDataProviderRequest.queryString(parameters);
-		}
-	}
-
-	protected DDMDataProviderContext createDDMDataProviderContext(
-		DDMDataProviderInstance ddmDataProviderInstance) {
-
-		try {
-			DDMDataProvider ddmDataProvider =
-				ddmDataProviderTracker.getDDMDataProvider(
-					ddmDataProviderInstance.getType());
-
-			DDMForm ddmForm = DDMFormFactory.create(
-				ddmDataProvider.getSettings());
-
-			DDMFormValues ddmFormValues =
-				ddmFormValuesJSONDeserializer.deserialize(
-					ddmForm, ddmDataProviderInstance.getDefinition());
-
-			return new DDMDataProviderContext(ddmFormValues);
-		}
-		catch (PortalException pe) {
-			throw new IllegalStateException(pe);
-		}
-	}
-
 	protected DDMDataProviderResponse createDDMDataProviderErrorResponse(
 		Exception e) {
+
+		DDMDataProviderResponse.Builder builder =
+			DDMDataProviderResponse.Builder.newBuilder();
 
 		if (e instanceof HystrixRuntimeException) {
 			FailureType failureType = getHystrixFailureType(e);
 
 			if (failureType == FailureType.TIMEOUT) {
-				return DDMDataProviderResponse.error(Status.TIMEOUT);
+				builder = builder.withStatus(
+					DDMDataProviderResponseStatus.TIMEOUT);
 			}
 			else if (failureType == FailureType.SHORTCIRCUIT) {
-				return DDMDataProviderResponse.error(Status.SHORTCIRCUIT);
+				builder = builder.withStatus(
+					DDMDataProviderResponseStatus.SHORT_CIRCUIT);
 			}
 		}
 		else if (e instanceof PrincipalException) {
-			return DDMDataProviderResponse.error(Status.UNAUTHORIZED);
+			builder = builder.withStatus(
+				DDMDataProviderResponseStatus.UNAUTHORIZED);
+		}
+		else {
+			builder = builder.withStatus(
+				DDMDataProviderResponseStatus.UNKNOWN_ERROR);
 		}
 
-		return DDMDataProviderResponse.error(Status.UNKNOWN_ERROR);
+		return builder.build();
 	}
 
 	protected DDMDataProviderResponse doInvoke(
 			DDMDataProviderRequest ddmDataProviderRequest)
 		throws Exception {
 
-		String ddmDataProviderInstanceId =
-			ddmDataProviderRequest.getDDMDataProviderInstanceId();
+		String ddmDataProviderId =
+			ddmDataProviderRequest.getDDMDataProviderId();
 
 		Optional<DDMDataProviderInstance> ddmDataProviderInstanceOptional =
-			fetchDDMDataProviderInstance(ddmDataProviderInstanceId);
-
-		setDDMDataProviderRequestAttributes(
-			ddmDataProviderRequest, ddmDataProviderInstanceOptional);
+			fetchDDMDataProviderInstance(ddmDataProviderId);
 
 		DDMDataProvider ddmDataProvider = getDDMDataProvider(
-			ddmDataProviderInstanceId, ddmDataProviderInstanceOptional);
+			ddmDataProviderId, ddmDataProviderInstanceOptional);
 
 		if (ddmDataProviderInstanceOptional.isPresent()) {
 			return doInvokeExternal(
@@ -216,44 +166,42 @@ public class DDMDataProviderInvokerImpl implements DDMDataProviderInvoker {
 		return hystrixRuntimeException.getFailureType();
 	}
 
-	protected void setDDMDataProviderRequestAttributes(
-		DDMDataProviderRequest ddmDataProviderRequest,
-		Optional<DDMDataProviderInstance> ddmDataProviderInstanceOptional) {
-
-		ddmDataProviderInstanceOptional.ifPresent(
-			ddmDataProviderInstance -> {
-				addDDMDataProviderRequestParameters(
-					ddmDataProviderRequest, ddmDataProviderInstance);
-
-				setDDMDataProviderRequestContext(
-					ddmDataProviderRequest, ddmDataProviderInstance);
-			});
-	}
-
-	protected void setDDMDataProviderRequestContext(
-		DDMDataProviderRequest ddmDataProviderRequest,
-		DDMDataProviderInstance ddmDataProviderInstance) {
-
-		DDMDataProviderContext ddmDataProviderContext =
-			createDDMDataProviderContext(ddmDataProviderInstance);
-
-		ddmDataProviderRequest.setDDMDataProviderContext(
-			ddmDataProviderContext);
-
-		// Backwards compatibility
-
-		ddmDataProviderContext.addParameters(
-			ddmDataProviderRequest.getParameters());
-	}
-
 	@Reference
 	protected DDMDataProviderInstanceService ddmDataProviderInstanceService;
 
 	@Reference
 	protected DDMDataProviderTracker ddmDataProviderTracker;
 
-	@Reference
-	protected DDMFormValuesJSONDeserializer ddmFormValuesJSONDeserializer;
+	protected static class DDMDataProviderInvokeCommand
+		extends HystrixCommand<DDMDataProviderResponse> {
+
+		public DDMDataProviderInvokeCommand(
+			String ddmDataProviderInstanceName, DDMDataProvider ddmDataProvider,
+			DDMDataProviderRequest ddmDataProviderRequest) {
+
+			super(
+				Setter.withGroupKey(_hystrixCommandGroupKey).andCommandKey(
+					HystrixCommandKey.Factory.asKey(
+						"DDMDataProviderInvokeCommand#" +
+							ddmDataProviderInstanceName)));
+
+			_ddmDataProvider = ddmDataProvider;
+			_ddmDataProviderRequest = ddmDataProviderRequest;
+		}
+
+		@Override
+		protected DDMDataProviderResponse run() throws Exception {
+			return _ddmDataProvider.getData(_ddmDataProviderRequest);
+		}
+
+		private static final HystrixCommandGroupKey _hystrixCommandGroupKey =
+			HystrixCommandGroupKey.Factory.asKey(
+				"DDMDataProviderInvokeCommandGroup");
+
+		private final DDMDataProvider _ddmDataProvider;
+		private final DDMDataProviderRequest _ddmDataProviderRequest;
+
+	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DDMDataProviderInvokerImpl.class);
