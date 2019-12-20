bdb8f08795cd7749a6d9a78f0d1882b2bce42ff7
Inácio Nery <inacio.nery@liferay.com>
2019-12-20 18:04:25 -0300

    LPS-106125 Add Portal Workflow Demo Data Creator API

---
+++ modules/apps/portal-workflow/portal-workflow-demo-data-creator-api/src/main/java/com/liferay/portal/workflow/demo/data/creator/WorkflowDefinitionDemoDataCreator.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.demo.data.creator;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.workflow.WorkflowDefinition;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Inácio Nery
+ */
+@ProviderType
+public interface WorkflowDefinitionDemoDataCreator {
+
+	public WorkflowDefinition create(long companyId) throws PortalException;
+
+	public void deleted() throws PortalException;
+
+}
