119920c29db858a305824b565304a17036697ee3
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-03-17 11:37:39 -0300

    LPS-107004 Add and map new DTOs exceptions

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/jaxrs/exception/mapper/NoSuchNodeExceptionMapper.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+import com.liferay.portal.workflow.metrics.rest.exception.v1_0.NoSuchNodeException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Portal.Workflow.Metrics.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Portal.Workflow.Metrics.REST.NoSuchNodeException"
+	},
+	service = ExceptionMapper.class
+)
+public class NoSuchNodeExceptionMapper
+	extends BaseExceptionMapper<NoSuchNodeException> {
+
+	@Override
+	protected Problem getProblem(NoSuchNodeException noSuchNodeException) {
+		return new Problem(
+			Response.Status.NOT_FOUND, noSuchNodeException.getMessage());
+	}
+
+}
