e2f3a8e070adeaa02dd3d214d4fb42a6119d3b62
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-03-20 17:17:57 -0300

    LPS-107004 Apply backend data model changes in front end

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/instance-list-page/InstanceListPageItem.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/instance-list-page/InstanceListPageItem.es.js
@@ -157,7 +157,7 @@ const Item = ({totalCount, ...instance}) => {
 				{formattedAssignees}
 			</ClayTable.Cell>
 
-			<ClayTable.Cell data-testid="creatorUserCell">
+			<ClayTable.Cell data-testid="creatorCell">
 				{creator ? creator.name : ''}
 			</ClayTable.Cell>
 
