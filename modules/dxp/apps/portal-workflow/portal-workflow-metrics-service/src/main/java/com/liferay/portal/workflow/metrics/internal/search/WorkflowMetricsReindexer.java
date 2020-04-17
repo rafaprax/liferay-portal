bc1a9dff56c2a0a3290c50cd7aad831f36d977a7
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-04-17 14:04:29 -0300

    LPS-107777 | LPS-110480 Keep reindex on startup

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/WorkflowMetricsReindexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/WorkflowMetricsReindexer.java
@@ -19,21 +19,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.search.BaseIndexer;
-import com.liferay.portal.kernel.search.Document;
-import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.workflow.metrics.internal.search.index.WorkflowMetricsIndex;
-import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexer;
-
-import java.util.Locale;
-
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -42,15 +29,8 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Inácio Nery
  */
-@Component(
-	immediate = true, service = {Indexer.class, WorkflowMetricsIndexer.class}
-)
-public class WorkflowMetricsIndexer extends BaseIndexer<Object> {
-
-	@Override
-	public String getClassName() {
-		return WorkflowMetricsIndexer.class.getName();
-	}
+@Component(immediate = true, service = WorkflowMetricsReindexer.class)
+public class WorkflowMetricsReindexer {
 
 	@Activate
 	protected void activate() throws Exception {
@@ -58,49 +38,26 @@ public class WorkflowMetricsIndexer extends BaseIndexer<Object> {
 			_companyLocalService.getActionableDynamicQuery();
 
 		actionableDynamicQuery.setPerformActionMethod(
-			(Company company) -> {
-				if (_INDEX_ON_STARTUP) {
-					return;
-				}
-
-				doReindex(company.getCompanyId());
-
-				_slaWorkflowMetricsIndexer.doReindex(company.getCompanyId());
-			});
+			(Company company) -> reindex(company.getCompanyId()));
 
 		actionableDynamicQuery.performActions();
 	}
 
-	@Override
-	protected final void doDelete(Object t) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected final Document doGetDocument(Object object) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected final Summary doGetSummary(
-			Document document, Locale locale, String snippet,
-			PortletRequest portletRequest, PortletResponse portletResponse)
-		throws Exception {
-
-		throw new UnsupportedOperationException();
-	}
-
-	protected void doReindex(long companyId) {
+	protected void reindex(long companyId) {
 		try {
 			_instanceWorkflowMetricsIndex.clearIndex(companyId);
 			_nodeWorkflowMetricsIndex.clearIndex(companyId);
 			_processWorkflowMetricsIndex.clearIndex(companyId);
+			_slaInstanceResultWorkflowMetricsIndex.clearIndex(companyId);
+			_slaTaskResultWorkflowMetricsIndex.clearIndex(companyId);
 			_taskWorkflowMetricsIndex.clearIndex(companyId);
 			_transitionWorkflowMetricsIndex.clearIndex(companyId);
 
 			_instanceWorkflowMetricsIndex.createIndex(companyId);
 			_nodeWorkflowMetricsIndex.createIndex(companyId);
 			_processWorkflowMetricsIndex.createIndex(companyId);
+			_slaInstanceResultWorkflowMetricsIndex.createIndex(companyId);
+			_slaTaskResultWorkflowMetricsIndex.createIndex(companyId);
 			_taskWorkflowMetricsIndex.createIndex(companyId);
 			_transitionWorkflowMetricsIndex.createIndex(companyId);
 
@@ -108,34 +65,18 @@ public class WorkflowMetricsIndexer extends BaseIndexer<Object> {
 			_nodeWorkflowMetricsReindexer.reindex(companyId);
 			_processWorkflowMetricsReindexer.reindex(companyId);
 			_taskWorkflowMetricsReindexer.reindex(companyId);
+			_transitionWorkflowMetricsReindexer.reindex(companyId);
+
+			_slaInstanceResultWorkflowMetricsReindexer.reindex(companyId);
+			_slaTaskResultWorkflowMetricsReindexer.reindex(companyId);
 		}
 		catch (PortalException portalException) {
 			_log.error(portalException, portalException);
 		}
 	}
 
-	@Override
-	protected final void doReindex(Object object) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected final void doReindex(String className, long classPK)
-		throws Exception {
-
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected void doReindex(String[] ids) throws Exception {
-		doReindex(GetterUtil.getLong(ids[0]));
-	}
-
-	private static final boolean _INDEX_ON_STARTUP = GetterUtil.getBoolean(
-		PropsUtil.get(PropsKeys.INDEX_ON_STARTUP));
-
 	private static final Log _log = LogFactoryUtil.getLog(
-		WorkflowMetricsIndexer.class);
+		WorkflowMetricsReindexer.class);
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
@@ -158,8 +99,21 @@ public class WorkflowMetricsIndexer extends BaseIndexer<Object> {
 	@Reference(target = "(workflow.metrics.index.entity.name=process)")
 	private WorkflowMetricsReindexer _processWorkflowMetricsReindexer;
 
-	@Reference
-	private SLAWorkflowMetricsIndexer _slaWorkflowMetricsIndexer;
+	@Reference(
+		target = "(workflow.metrics.index.entity.name=sla-instance-result)"
+	)
+	private WorkflowMetricsIndex _slaInstanceResultWorkflowMetricsIndex;
+
+	@Reference(
+		target = "(workflow.metrics.index.entity.name=sla-instance-result)"
+	)
+	private WorkflowMetricsReindexer _slaInstanceResultWorkflowMetricsReindexer;
+
+	@Reference(target = "(workflow.metrics.index.entity.name=sla-task-result)")
+	private WorkflowMetricsIndex _slaTaskResultWorkflowMetricsIndex;
+
+	@Reference(target = "(workflow.metrics.index.entity.name=sla-task-result)")
+	private WorkflowMetricsReindexer _slaTaskResultWorkflowMetricsReindexer;
 
 	@Reference(target = "(workflow.metrics.index.entity.name=task)")
 	private WorkflowMetricsIndex _taskWorkflowMetricsIndex;
