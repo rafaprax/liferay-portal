56569eaf192ed4e23a6cee4e70e35c5eee7902b2
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-09-09 12:24:48 -0300

    WIP

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/SLAInstanceResultWorkflowMetricsIndexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/SLAInstanceResultWorkflowMetricsIndexer.java
@@ -14,13 +14,30 @@
 
 package com.liferay.portal.workflow.metrics.internal.search.index;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+import com.liferay.portal.kernel.util.PortalRunMode;
 import com.liferay.portal.search.document.Document;
 import com.liferay.portal.search.document.DocumentBuilder;
+import com.liferay.portal.search.engine.adapter.document.UpdateByQueryDocumentRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.workflow.metrics.internal.messaging.WorkflowMetricsSLAProcessMessageListener;
+import com.liferay.portal.workflow.metrics.internal.sla.WorkflowMetricsInstanceSLAStatus;
 import com.liferay.portal.workflow.metrics.internal.sla.processor.WorkflowMetricsSLAInstanceResult;
 import com.liferay.portal.workflow.metrics.sla.processor.WorkflowMetricsSLAStatus;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Rafael Praxedes
@@ -123,6 +140,60 @@ public class SLAInstanceResultWorkflowMetricsIndexer
 	}
 
 	@Override
+	public void deleteDocuments(
+		long companyId, long processId, long slaDefinitionId) {
+
+		super.deleteDocuments(companyId, processId, slaDefinitionId);
+
+		BooleanQuery booleanQuery = queries.booleanQuery();
+
+		BooleanQuery filterBooleanQuery = queries.booleanQuery();
+
+		filterBooleanQuery.addMustNotQueryClauses(
+			queries.term("slaDefinitionId", 0));
+		filterBooleanQuery.addMustQueryClauses(
+			queries.term("completed", false),
+			queries.term("processId", processId));
+
+		booleanQuery.addFilterQueryClauses(filterBooleanQuery);
+
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		searchSearchRequest.setQuery(booleanQuery);
+
+		UpdateByQueryDocumentRequest updateByQueryDocumentRequest =
+			new UpdateByQueryDocumentRequest(
+				booleanQuery,
+				scripts.script(
+					StringBundler.concat(
+						"ctx._source.slaStatus = ", StringPool.QUOTE,
+						WorkflowMetricsInstanceSLAStatus.UNTRACKED.getValue(),
+						StringPool.QUOTE)),
+				getIndexName(companyId));
+
+		if (PortalRunMode.isTestMode()) {
+			updateByQueryDocumentRequest.setRefresh(true);
+		}
+
+		searchEngineAdapter.execute(updateByQueryDocumentRequest);
+
+		try {
+			Message message = new Message();
+
+			JSONObject payloadJSONObject = _jsonFactory.createJSONObject();
+
+			payloadJSONObject.put("processId", processId);
+
+			message.setPayload(payloadJSONObject);
+
+			workflowMetricsSLAProcessMessageListener.receive(message);
+		}
+		catch (MessageListenerException messageListenerException) {
+			_log.error(messageListenerException, messageListenerException);
+		}
+	}
+
+	@Override
 	public String getIndexName(long companyId) {
 		return _slaInstanceResultWorkflowMetricsIndex.getIndexName(companyId);
 	}
@@ -131,6 +202,23 @@ public class SLAInstanceResultWorkflowMetricsIndexer
 	public String getIndexType() {
 		return _slaInstanceResultWorkflowMetricsIndex.getIndexType();
 	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	protected volatile WorkflowMetricsSLAProcessMessageListener
+		workflowMetricsSLAProcessMessageListener;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SLAInstanceResultWorkflowMetricsIndexer.class);
+
+	@Reference(target = "(workflow.metrics.index.entity.name=instance)")
+	private WorkflowMetricsIndex _instanceWorkflowMetricsIndex;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference(
 		target = "(workflow.metrics.index.entity.name=sla-instance-result)"
