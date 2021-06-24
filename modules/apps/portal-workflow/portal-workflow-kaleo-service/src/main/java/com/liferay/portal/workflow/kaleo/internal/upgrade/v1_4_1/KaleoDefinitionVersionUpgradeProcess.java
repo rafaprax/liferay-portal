9db01dafc54d0366a399bfb1a2dea823076f4cf4
jesseyeh-liferay <jesse.yeh@liferay.com>
2021-06-23 17:10:35 -0700

    LPS-107816 Check if KaleoDraftDefinition table exists

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/v1_4_1/KaleoDefinitionVersionUpgradeProcess.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/v1_4_1/KaleoDefinitionVersionUpgradeProcess.java
@@ -16,6 +16,7 @@ package com.liferay.portal.workflow.kaleo.internal.upgrade.v1_4_1;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
@@ -25,6 +26,7 @@ import com.liferay.portal.workflow.kaleo.internal.upgrade.v1_4_1.util.KaleoDefin
 
 import java.io.IOException;
 
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -194,47 +196,60 @@ public class KaleoDefinitionVersionUpgradeProcess extends UpgradeProcess {
 				}
 			}
 
-			PreparedStatement preparedStatement3 = connection.prepareStatement(
-				"select * from KaleoDraftDefinition where draftVersion != 1");
+			DBInspector dbInspector = new DBInspector(connection);
 
-			ResultSet resultSet2 = preparedStatement3.executeQuery();
+			DatabaseMetaData metaData = connection.getMetaData();
 
-			while (resultSet2.next()) {
-				long kaleoDefinitionVersionId = increment();
-				long groupId = resultSet2.getLong("groupId");
-				long companyId = resultSet2.getLong("companyId");
-				long userId = resultSet2.getLong("userId");
-				String userName = resultSet2.getString("userName");
-				Timestamp createDate = resultSet2.getTimestamp("createDate");
-				Timestamp modifiedDate = resultSet2.getTimestamp(
-					"modifiedDate");
-				String name = resultSet2.getString("name");
-				String title = resultSet2.getString("title");
-				String content = resultSet2.getString("content");
-				int version = resultSet2.getInt("version");
-				int draftVersion = resultSet2.getInt("draftVersion");
+			ResultSet metaDataResultSet = metaData.getTables(
+				dbInspector.getCatalog(), dbInspector.getSchema(),
+				"KaleoDraftDefinition", new String[] {"TABLE"});
 
-				preparedStatement2.setLong(1, kaleoDefinitionVersionId);
-				preparedStatement2.setLong(2, groupId);
-				preparedStatement2.setLong(3, companyId);
-				preparedStatement2.setLong(4, userId);
-				preparedStatement2.setString(5, userName);
-				preparedStatement2.setLong(6, userId);
-				preparedStatement2.setString(7, userName);
-				preparedStatement2.setTimestamp(8, modifiedDate);
-				preparedStatement2.setTimestamp(9, createDate);
-				preparedStatement2.setTimestamp(10, modifiedDate);
-				preparedStatement2.setString(11, name);
-				preparedStatement2.setString(12, title);
-				preparedStatement2.setString(13, StringPool.BLANK);
-				preparedStatement2.setString(14, content);
-				preparedStatement2.setString(
-					15, getVersion(version, draftVersion));
-				preparedStatement2.setLong(16, 0);
-				preparedStatement2.setInt(
-					17, WorkflowConstants.STATUS_APPROVED);
+			if (metaDataResultSet.next()) {
+				PreparedStatement preparedStatement3 =
+					connection.prepareStatement(
+						"select * from KaleoDraftDefinition where " +
+							"draftVersion != 1");
 
-				preparedStatement2.addBatch();
+				ResultSet resultSet2 = preparedStatement3.executeQuery();
+
+				while (resultSet2.next()) {
+					long kaleoDefinitionVersionId = increment();
+					long groupId = resultSet2.getLong("groupId");
+					long companyId = resultSet2.getLong("companyId");
+					long userId = resultSet2.getLong("userId");
+					String userName = resultSet2.getString("userName");
+					Timestamp createDate = resultSet2.getTimestamp(
+						"createDate");
+					Timestamp modifiedDate = resultSet2.getTimestamp(
+						"modifiedDate");
+					String name = resultSet2.getString("name");
+					String title = resultSet2.getString("title");
+					String content = resultSet2.getString("content");
+					int version = resultSet2.getInt("version");
+					int draftVersion = resultSet2.getInt("draftVersion");
+
+					preparedStatement2.setLong(1, kaleoDefinitionVersionId);
+					preparedStatement2.setLong(2, groupId);
+					preparedStatement2.setLong(3, companyId);
+					preparedStatement2.setLong(4, userId);
+					preparedStatement2.setString(5, userName);
+					preparedStatement2.setLong(6, userId);
+					preparedStatement2.setString(7, userName);
+					preparedStatement2.setTimestamp(8, modifiedDate);
+					preparedStatement2.setTimestamp(9, createDate);
+					preparedStatement2.setTimestamp(10, modifiedDate);
+					preparedStatement2.setString(11, name);
+					preparedStatement2.setString(12, title);
+					preparedStatement2.setString(13, StringPool.BLANK);
+					preparedStatement2.setString(14, content);
+					preparedStatement2.setString(
+						15, getVersion(version, draftVersion));
+					preparedStatement2.setLong(16, 0);
+					preparedStatement2.setInt(
+						17, WorkflowConstants.STATUS_APPROVED);
+
+					preparedStatement2.addBatch();
+				}
 			}
 
 			preparedStatement2.executeBatch();
