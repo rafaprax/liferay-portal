47b68fff225e6005a71f1d5452b4daaa8e57f335
jesseyeh-liferay <jesse.yeh@liferay.com>
2021-06-30 10:20:18 -0700

    LPS-107816 Remove unnecessary upgrade step

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/upgrade/KaleoDesignerWebUpgrade.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/upgrade/KaleoDesignerWebUpgrade.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.workflow.kaleo.designer.web.internal.upgrade.v1_0_0.UpgradePortletId;
-import com.liferay.portal.workflow.kaleo.designer.web.internal.upgrade.v1_0_1.KaleoDefinitionVersionUpgradeProcess;
 import com.liferay.portal.workflow.kaleo.designer.web.internal.upgrade.v1_0_2.KaleoDefinitionUpgradeProcess;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
@@ -40,11 +39,7 @@ public class KaleoDesignerWebUpgrade implements UpgradeStepRegistrator {
 
 		registry.register("0.0.1", "1.0.0", new UpgradePortletId());
 
-		registry.register(
-			"1.0.0", "1.0.1",
-			new KaleoDefinitionVersionUpgradeProcess(
-				_counterLocalService, _kaleoDefinitionLocalService,
-				_kaleoDefinitionVersionLocalService, _userLocalService));
+		registry.register("1.0.0", "1.0.1", new DummyUpgradeStep());
 
 		registry.register(
 			"1.0.1", "1.0.2",
