6aa25de4cf47542feb69ed1253564227a1076194
Rafael Praxedes <rafael.praxedes@liferay.com>
2021-12-17 19:38:36 -0300

    LPS-144170 Move usages of gif89.jar, im4java.jar and monte-cc.jar to image-impl module

--- modules/apps/image/image-impl/src/test/java/com/liferay/image/internal/ImageToolImplTest.java
+++ modules/apps/image/image-impl/src/test/java/com/liferay/image/internal/ImageToolImplTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.image;
+package com.liferay.image.internal;
 
 import com.liferay.portal.kernel.image.ImageBag;
 import com.liferay.portal.kernel.image.ImageTool;
