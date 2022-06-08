704819db0504795cc03bc7fa426fdcf58810c4cb
Rafael Praxedes <rafael.praxedes@liferay.com>
2022-06-08 15:53:03 -0300

    LPS-155543 Move tests

--- modules/apps/portal-remote/portal-remote-json-web-service-web/src/test/java/com/liferay/portal/remote/json/web/service/web/internal/FooService.java
+++ modules/apps/portal-remote/portal-remote-json-web-service-web/src/test/java/com/liferay/portal/remote/json/web/service/web/internal/FooService.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.jsonwebservice;
+package com.liferay.portal.remote.json.web.service.web.internal;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -218,8 +218,8 @@ public class FooService {
 		return sb.toString();
 	}
 
-	public static String use1(FooDataImpl fooData) {
-		return "using #1: " + fooData.toString();
+	public static String use1(FooDataImpl fooDataImpl) {
+		return "using #1: " + fooDataImpl.toString();
 	}
 
 	public static String use2(FooData fooData) {
