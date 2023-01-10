c90adacb3c64ec864d42551ddb296384bf704c81
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2023-01-10 18:43:20 -0300

    LPS-172511 Remove immediate from dynamic-data-mapping-form-taglib module

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-taglib/src/main/java/com/liferay/dynamic/data/mapping/form/taglib/internal/servlet/ServletContextUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-taglib/src/main/java/com/liferay/dynamic/data/mapping/form/taglib/internal/servlet/ServletContextUtil.java
@@ -25,7 +25,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Rafael Praxedes
  * @generated
  */
-@Component(immediate = true)
+@Component
 public class ServletContextUtil {
 
 	public static ServletContext getServletContext() {
