8c283604790f0f1e61bcd253089ca981da5ba20d
valmir junior <valmiralvesjr2000@hotmail.com>
2023-08-31 15:11:21 -0300

    LPS-194841 Rename package

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/interpreter/SearchResultInterpreterProviderImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/interpreter/SearchResultInterpreterProviderImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.web.internal.Interpreter;
+package com.liferay.portal.search.web.internal.interpreter;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
