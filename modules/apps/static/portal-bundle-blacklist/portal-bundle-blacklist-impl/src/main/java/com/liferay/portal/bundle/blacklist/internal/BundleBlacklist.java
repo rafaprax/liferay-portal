328211cba4a9bbae5c3f6d3d2880753ae037afab
Rafael Praxedes <rafael.praxedes@liferay.com>
2023-09-06 10:41:38 -0300

    LPS-192723 Inline BundleBlacklistManagerImpl into the BundleBlacklist class, prepare for next

--- modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist-impl/src/main/java/com/liferay/portal/bundle/blacklist/internal/BundleBlacklist.java
+++ modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist-impl/src/main/java/com/liferay/portal/bundle/blacklist/internal/BundleBlacklist.java
@@ -6,10 +6,15 @@
 package com.liferay.portal.bundle.blacklist.internal;
 
 import com.liferay.osgi.util.BundleUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.bundle.blacklist.BundleBlacklistManager;
 import com.liferay.portal.bundle.blacklist.internal.configuration.BundleBlacklistConfiguration;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.lpkg.deployer.LPKGDeployer;
 
 import java.io.File;
@@ -21,6 +26,9 @@ import java.io.OutputStream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Dictionary;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -28,6 +36,8 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,10 +45,18 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.framework.SynchronousBundleListener;
 import org.osgi.framework.startlevel.BundleStartLevel;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
@@ -51,13 +69,25 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class BundleBlacklist {
 
-	public List<String> getBlacklistBundleSymbolicNames() {
-		return new ArrayList<>(_uninstalledBundles.keySet());
+	@Activate
+	protected void activate(
+			BundleContext bundleContext, Map<String, String> properties)
+		throws Exception {
+
+		modified(bundleContext, properties);
+
+		_serviceRegistration = bundleContext.registerService(
+			BundleBlacklistManager.class, new BundleBlacklistManagerImpl(),
+			null);
 	}
 
-	@Activate
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
 	@Modified
-	protected void activate(
+	protected void modified(
 			BundleContext bundleContext, Map<String, String> properties)
 		throws Exception {
 
@@ -266,10 +296,173 @@ public class BundleBlacklist {
 		};
 
 	@Reference
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
 	private LPKGDeployer _lpkgDeployer;
 
 	private volatile BundleListener _selfMonitorBundleListener;
+	private ServiceRegistration<BundleBlacklistManager> _serviceRegistration;
 	private final Map<String, UninstalledBundleData> _uninstalledBundles =
 		new ConcurrentHashMap<>();
+
+	private class BundleBlacklistManagerImpl implements BundleBlacklistManager {
+
+		@Override
+		public void addToBlacklistAndUninstall(String... bundleSymbolicNames)
+			throws IOException {
+
+			_updateProperties(
+				blacklistBundleSymbolicNames -> {
+					if (blacklistBundleSymbolicNames == null) {
+						return bundleSymbolicNames;
+					}
+
+					Set<String> blacklistBundleSymbolicNamesSet =
+						SetUtil.fromArray(blacklistBundleSymbolicNames);
+
+					Collections.addAll(
+						blacklistBundleSymbolicNamesSet, bundleSymbolicNames);
+
+					return blacklistBundleSymbolicNamesSet.toArray(
+						new String[0]);
+				});
+		}
+
+		@Override
+		public Collection<String> getBlacklistBundleSymbolicNames() {
+			return new ArrayList<>(_uninstalledBundles.keySet());
+		}
+
+		@Override
+		public void removeFromBlacklistAndInstall(String... bundleSymbolicNames)
+			throws IOException {
+
+			_updateProperties(
+				blacklistBundleSymbolicNames -> {
+					if (blacklistBundleSymbolicNames == null) {
+						return null;
+					}
+
+					Set<String> blacklistBundleSymbolicNamesSet =
+						SetUtil.fromArray(blacklistBundleSymbolicNames);
+
+					for (String bundleSymbolicName : bundleSymbolicNames) {
+						blacklistBundleSymbolicNamesSet.remove(
+							bundleSymbolicName);
+					}
+
+					return blacklistBundleSymbolicNamesSet.toArray(
+						new String[0]);
+				});
+		}
+
+		private void _updateConfiguration(
+				Configuration configuration,
+				Dictionary<String, Object> properties)
+			throws IOException {
+
+			Bundle bundle = FrameworkUtil.getBundle(
+				BundleBlacklistManager.class);
+
+			BundleContext bundleContext = bundle.getBundleContext();
+
+			CountDownLatch countDownLatch = new CountDownLatch(1);
+
+			ServiceListener serviceListener = new ServiceListener() {
+
+				@Override
+				public void serviceChanged(ServiceEvent serviceEvent) {
+					if (serviceEvent.getType() != ServiceEvent.MODIFIED) {
+						return;
+					}
+
+					ServiceReference<?> serviceReference =
+						serviceEvent.getServiceReference();
+
+					Object service = bundleContext.getService(serviceReference);
+
+					Class<?> serviceClass = service.getClass();
+
+					if (BundleBlacklist.class.getName() ==
+							serviceClass.getName()) {
+
+						countDownLatch.countDown();
+					}
+
+					bundleContext.ungetService(serviceReference);
+				}
+
+			};
+
+			bundleContext.addServiceListener(serviceListener);
+
+			try {
+				configuration.update(properties);
+
+				countDownLatch.await();
+			}
+			catch (InterruptedException interruptedException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(interruptedException);
+				}
+			}
+			finally {
+				bundleContext.removeServiceListener(serviceListener);
+			}
+		}
+
+		private void _updateProperties(
+				Function<String[], String[]> updateFunction)
+			throws IOException {
+
+			Configuration configuration = _configurationAdmin.getConfiguration(
+				BundleBlacklistConfiguration.class.getName(),
+				StringPool.QUESTION);
+
+			Dictionary<String, Object> properties =
+				configuration.getProperties();
+
+			String[] blacklistBundleSymbolicNames = null;
+
+			if (properties == null) {
+				properties = new HashMapDictionary<>();
+			}
+			else {
+
+				// LPS-114840
+
+				Object value = properties.get("blacklistBundleSymbolicNames");
+
+				if (value instanceof String) {
+					blacklistBundleSymbolicNames = StringUtil.split(
+						(String)value);
+				}
+				else {
+					blacklistBundleSymbolicNames = (String[])properties.get(
+						"blacklistBundleSymbolicNames");
+				}
+			}
+
+			blacklistBundleSymbolicNames = updateFunction.apply(
+				blacklistBundleSymbolicNames);
+
+			if (blacklistBundleSymbolicNames == null) {
+				return;
+			}
+
+			if (blacklistBundleSymbolicNames.length == 0) {
+				properties.remove("blacklistBundleSymbolicNames");
+			}
+			else {
+				properties.put(
+					"blacklistBundleSymbolicNames",
+					blacklistBundleSymbolicNames);
+			}
+
+			_updateConfiguration(configuration, properties);
+		}
+
+	}
 
 }
