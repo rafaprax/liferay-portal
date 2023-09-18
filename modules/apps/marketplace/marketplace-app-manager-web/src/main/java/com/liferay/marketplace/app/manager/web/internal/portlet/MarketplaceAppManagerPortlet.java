65940b53f6d7a0e27cd05268ced9dd2eca1b37b9
Rafael Praxedes <rafael.praxedes@liferay.com>
2023-09-18 17:23:18 -0300

    LPS-192723 Inline BundleBlacklistManagerImpl into MarketplaceAppManagerPortlet

--- modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/internal/portlet/MarketplaceAppManagerPortlet.java
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/internal/portlet/MarketplaceAppManagerPortlet.java
@@ -16,7 +16,9 @@ import com.liferay.marketplace.exception.FileExtensionException;
 import com.liferay.marketplace.service.AppService;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.bundle.blacklist.BundleBlacklistManager;
+import com.liferay.portal.bundle.blacklist.constants.BundleBlacklistConstants;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.LayoutTemplate;
 import com.liferay.portal.kernel.model.Plugin;
 import com.liferay.portal.kernel.model.PluginSetting;
@@ -38,11 +40,14 @@ import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -54,7 +59,12 @@ import java.net.URL;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Function;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -68,8 +78,15 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 
 /**
  * @author Ryan Park
@@ -480,14 +497,20 @@ public class MarketplaceAppManagerPortlet extends MVCPortlet {
 		}
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		MarketplaceAppManagerPortlet.class);
+
 	@Reference
 	private AppService _appService;
 
-	@Reference
-	private BundleBlacklistManager _bundleBlacklistManager;
+	private final BundleBlacklistManager _bundleBlacklistManager =
+		new BundleBlacklistManager();
 
 	@Reference
 	private BundleManager _bundleManager;
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
 
 	@Reference
 	private Http _http;
@@ -509,5 +532,118 @@ public class MarketplaceAppManagerPortlet extends MVCPortlet {
 
 	@Reference
 	private PortletService _portletService;
+
+	private class BundleBlacklistManager {
+
+		public void addToBlacklistAndUninstall(String... bundleSymbolicNames)
+			throws IOException {
+
+			_updateProperties(
+				blacklistBundleSymbolicNames -> {
+					if (blacklistBundleSymbolicNames == null) {
+						return bundleSymbolicNames;
+					}
+
+					Set<String> blacklistBundleSymbolicNamesSet =
+						SetUtil.fromArray(blacklistBundleSymbolicNames);
+
+					Collections.addAll(
+						blacklistBundleSymbolicNamesSet, bundleSymbolicNames);
+
+					return blacklistBundleSymbolicNamesSet.toArray(
+						new String[0]);
+				});
+		}
+
+		private void _updateConfiguration(
+				Configuration configuration,
+				Dictionary<String, Object> properties)
+			throws IOException {
+
+			Bundle bundle = FrameworkUtil.getBundle(
+				BundleBlacklistManager.class);
+
+			BundleContext bundleContext = bundle.getBundleContext();
+
+			CountDownLatch countDownLatch = new CountDownLatch(1);
+
+			ServiceRegistration<EventHandler> eventHandlerServiceRegistration =
+				bundleContext.registerService(
+					EventHandler.class, event -> countDownLatch.countDown(),
+					MapUtil.singletonDictionary(
+						EventConstants.EVENT_TOPIC,
+						BundleBlacklistConstants.
+							TOPIC_BUNDLE_BLACKLIST_FINISHED));
+
+			try {
+				configuration.update(properties);
+
+				countDownLatch.await();
+			}
+			catch (InterruptedException interruptedException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(interruptedException);
+				}
+			}
+			finally {
+				eventHandlerServiceRegistration.unregister();
+			}
+		}
+
+		private void _updateProperties(
+				Function<String[], String[]> updateFunction)
+			throws IOException {
+
+			Configuration configuration = _configurationAdmin.getConfiguration(
+				_BUNDLE_BLACKLIST_CONFIGURATION_PID, StringPool.QUESTION);
+
+			Dictionary<String, Object> properties =
+				configuration.getProperties();
+
+			String[] blacklistBundleSymbolicNames = null;
+
+			if (properties == null) {
+				properties = new HashMapDictionary<>();
+			}
+			else {
+
+				// LPS-114840
+
+				Object value = properties.get("blacklistBundleSymbolicNames");
+
+				if (value instanceof String) {
+					blacklistBundleSymbolicNames = StringUtil.split(
+						(String)value);
+				}
+				else {
+					blacklistBundleSymbolicNames = (String[])properties.get(
+						"blacklistBundleSymbolicNames");
+				}
+			}
+
+			blacklistBundleSymbolicNames = updateFunction.apply(
+				blacklistBundleSymbolicNames);
+
+			if (blacklistBundleSymbolicNames == null) {
+				return;
+			}
+
+			if (blacklistBundleSymbolicNames.length == 0) {
+				properties.remove("blacklistBundleSymbolicNames");
+			}
+			else {
+				properties.put(
+					"blacklistBundleSymbolicNames",
+					blacklistBundleSymbolicNames);
+			}
+
+			_updateConfiguration(configuration, properties);
+		}
+
+		private static final String _BUNDLE_BLACKLIST_CONFIGURATION_PID =
+			"com.liferay.portal.bundle.blacklist.internal.configuration." +
+				"BundleBlacklistConfiguration";
+
+	}
 
 }
--- modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist-test/src/testIntegration/java/com/liferay/portal/bundle/blacklist/test/BundleBlacklistTest.java
+++ modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist-test/src/testIntegration/java/com/liferay/portal/bundle/blacklist/test/BundleBlacklistTest.java
@@ -9,7 +9,6 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.osgi.util.service.OSGiServiceUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.bundle.blacklist.BundleBlacklistManager;
 import com.liferay.portal.bundle.blacklist.constants.BundleBlacklistConstants;
 import com.liferay.portal.kernel.module.util.BundleUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -17,7 +16,6 @@ import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.lpkg.deployer.test.util.LPKGTestUtil;
-import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PropsValues;
 
@@ -27,9 +25,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Dictionary;
 import java.util.concurrent.CountDownLatch;
 
@@ -144,46 +139,6 @@ public class BundleBlacklistTest {
 		countDownLatch.await();
 
 		_updateConfiguration(_properties);
-	}
-
-	@Test
-	public void testAddToAndRemoveFromBlacklist() throws Exception {
-		Bundle bundle = _findBundle(_SYMBOLIC_NAME);
-
-		Assert.assertEquals(Bundle.ACTIVE, bundle.getState());
-
-		Collection<String> blacklistBundleSymbolicNames =
-			_getBlacklistBundleSymbolicNames();
-
-		Assert.assertFalse(
-			_SYMBOLIC_NAME + " should not be blacklisted",
-			blacklistBundleSymbolicNames.contains(_SYMBOLIC_NAME));
-
-		_bundleBlacklistManager.addToBlacklistAndUninstall(_SYMBOLIC_NAME);
-
-		for (Bundle curBundle : _bundleContext.getBundles()) {
-			Assert.assertFalse(
-				curBundle + " should not be installed",
-				_SYMBOLIC_NAME.equals(curBundle.getSymbolicName()));
-		}
-
-		blacklistBundleSymbolicNames = _getBlacklistBundleSymbolicNames();
-
-		Assert.assertTrue(
-			_SYMBOLIC_NAME + " should be blacklisted",
-			blacklistBundleSymbolicNames.contains(_SYMBOLIC_NAME));
-
-		_bundleBlacklistManager.removeFromBlacklistAndInstall(_SYMBOLIC_NAME);
-
-		bundle = _findBundle(_SYMBOLIC_NAME);
-
-		Assert.assertEquals(Bundle.ACTIVE, bundle.getState());
-
-		blacklistBundleSymbolicNames = _getBlacklistBundleSymbolicNames();
-
-		Assert.assertFalse(
-			_SYMBOLIC_NAME + " should not be blacklisted",
-			blacklistBundleSymbolicNames.contains(_SYMBOLIC_NAME));
 	}
 
 	@Test
@@ -347,26 +302,6 @@ public class BundleBlacklistTest {
 		return bundle;
 	}
 
-	private Collection<String> _getBlacklistBundleSymbolicNames()
-		throws Exception {
-
-		Configuration configuration = OSGiServiceUtil.callService(
-			_bundleContext, ConfigurationAdmin.class,
-			configurationAdmin -> configurationAdmin.getConfiguration(
-				_CONFIG_NAME, StringPool.QUESTION));
-
-		Dictionary<String, Object> properties = configuration.getProperties();
-
-		String[] blacklistBundleSymbolicNames = (String[])properties.get(
-			_PROP_KEY);
-
-		if (blacklistBundleSymbolicNames == null) {
-			return Collections.emptyList();
-		}
-
-		return Arrays.asList(blacklistBundleSymbolicNames);
-	}
-
 	private void _updateConfiguration(Dictionary<String, Object> dictionary)
 		throws Exception {
 
@@ -404,9 +339,6 @@ public class BundleBlacklistTest {
 
 	private static final String _SYMBOLIC_NAME =
 		"com.liferay.portal.bundle.blacklist.test.bundle";
-
-	@Inject
-	private static BundleBlacklistManager _bundleBlacklistManager;
 
 	private Configuration _bundleBlacklistConfiguration;
 	private BundleContext _bundleContext;
