947cab7610a1ed1b9a32da19aa0eac6d1af33083
Regisson Aguiar <regisson.aguiar@liferay.com>
2023-12-12 17:27:38 -0300

    LPS-198805 rename ChannelController to ChannelFaroController, prepare for next

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/application/AsahApplication.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/application/AsahApplication.java
@@ -6,7 +6,7 @@
 package com.liferay.osb.faro.web.internal.application;
 
 import com.liferay.osb.faro.web.internal.constants.FaroConstants;
-import com.liferay.osb.faro.web.internal.controller.asah.ChannelController;
+import com.liferay.osb.faro.web.internal.controller.asah.ChannelFaroController;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,12 +28,12 @@ public class AsahApplication extends BaseApplication {
 	public Set<Object> getControllers() {
 		Set<Object> controllers = new HashSet<>();
 
-		controllers.add(_channelController);
+		controllers.add(_channelFaroController);
 
 		return controllers;
 	}
 
 	@Reference
-	private ChannelController _channelController;
+	private ChannelFaroController _channelFaroController;
 
 }
