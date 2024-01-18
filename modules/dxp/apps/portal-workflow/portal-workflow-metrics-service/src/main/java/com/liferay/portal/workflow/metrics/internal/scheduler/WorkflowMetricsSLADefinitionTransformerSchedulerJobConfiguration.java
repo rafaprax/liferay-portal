81dccc309dcf90e66f26990506405c9c2aeb2bd9
Valmir Junior <valmir.junior@liferay.com>
2024-01-18 14:09:44 -0300

    LPS-206371 Move methods from class WorkflowMetricsSLADefinitionTransformer to class WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/scheduler/WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/scheduler/WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration.java
@@ -7,6 +7,10 @@ package com.liferay.portal.workflow.metrics.internal.scheduler;
 
 import com.liferay.petra.function.UnsafeConsumer;
 import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -15,8 +19,20 @@ import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.aggregation.AggregationResult;
+import com.liferay.portal.search.aggregation.Aggregations;
+import com.liferay.portal.search.aggregation.bucket.Bucket;
+import com.liferay.portal.search.aggregation.bucket.TermsAggregation;
+import com.liferay.portal.search.aggregation.bucket.TermsAggregationResult;
+import com.liferay.portal.search.aggregation.metrics.TopHitsAggregation;
+import com.liferay.portal.search.aggregation.metrics.TopHitsAggregationResult;
 import com.liferay.portal.search.document.Document;
 import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.search.SearchRequestExecutor;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
 import com.liferay.portal.search.hits.SearchHit;
@@ -24,11 +40,17 @@ import com.liferay.portal.search.hits.SearchHits;
 import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.search.query.BooleanQuery;
 import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.query.TermsQuery;
 import com.liferay.portal.workflow.metrics.internal.configuration.WorkflowMetricsConfiguration;
-import com.liferay.portal.workflow.metrics.internal.sla.transformer.WorkflowMetricsSLADefinitionTransformer;
+import com.liferay.portal.workflow.metrics.model.WorkflowMetricsSLADefinition;
 import com.liferay.portal.workflow.metrics.search.index.WorkflowMetricsIndicesAvailabilityChecker;
 import com.liferay.portal.workflow.metrics.search.index.constants.WorkflowMetricsIndexNameConstants;
+import com.liferay.portal.workflow.metrics.service.WorkflowMetricsSLADefinitionLocalService;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Activate;
@@ -83,6 +105,116 @@ public class WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration
 			_queries.term("deleted", Boolean.FALSE));
 	}
 
+	private BooleanQuery _createNodeBooleanQuery(
+		String currentProcessVersion, String latestProcessVersion,
+		WorkflowMetricsSLADefinition workflowMetricsSLADefinition) {
+
+		BooleanQuery booleanQuery = _queries.booleanQuery();
+
+		TermsQuery termsQuery = _queries.terms("version");
+
+		termsQuery.addValues(currentProcessVersion, latestProcessVersion);
+
+		return booleanQuery.addMustQueryClauses(
+			_queries.term(
+				"companyId", workflowMetricsSLADefinition.getCompanyId()),
+			_queries.term(
+				"processId", workflowMetricsSLADefinition.getProcessId()),
+			termsQuery);
+	}
+
+	private String _getNodeId(
+		String processVersion,
+		TermsAggregationResult versionTermsAggregationResult) {
+
+		Bucket processVersionBucket = versionTermsAggregationResult.getBucket(
+			processVersion);
+
+		TopHitsAggregationResult topHitsAggregationResult =
+			(TopHitsAggregationResult)
+				processVersionBucket.getChildAggregationResult("topHits");
+
+		SearchHits searchHits = topHitsAggregationResult.getSearchHits();
+
+		for (SearchHit searchHit : searchHits.getSearchHits()) {
+			return MapUtil.getString(searchHit.getSourcesMap(), "nodeId");
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private Map<String, String> _getNodeIdMap(
+		String currentProcessVersion, String latestProcessVersion,
+		WorkflowMetricsSLADefinition workflowMetricsSLADefinition) {
+
+		Map<String, String> nodeIds = new HashMap<>();
+
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		TermsAggregation nameTermsAggregation = _aggregations.terms(
+			"name", "name");
+
+		nameTermsAggregation.setSize(10000);
+
+		TermsAggregation versionTermsAggregation = _aggregations.terms(
+			"version", "version");
+
+		TopHitsAggregation topHitsAggregation = _aggregations.topHits(
+			"topHits");
+
+		topHitsAggregation.setSize(2);
+
+		versionTermsAggregation.addChildAggregation(topHitsAggregation);
+
+		versionTermsAggregation.setSize(10000);
+
+		nameTermsAggregation.addChildAggregation(versionTermsAggregation);
+
+		searchSearchRequest.addAggregation(nameTermsAggregation);
+
+		String indexName = _indexNameBuilder.getIndexName(
+			workflowMetricsSLADefinition.getCompanyId());
+
+		searchSearchRequest.setIndexNames(
+			indexName + WorkflowMetricsIndexNameConstants.SUFFIX_NODE);
+
+		searchSearchRequest.setQuery(
+			_createNodeBooleanQuery(
+				currentProcessVersion, latestProcessVersion,
+				workflowMetricsSLADefinition));
+		searchSearchRequest.setSize(0);
+
+		SearchSearchResponse searchSearchResponse =
+			_searchRequestExecutor.executeSearchRequest(searchSearchRequest);
+
+		Map<String, AggregationResult> aggregationResultsMap =
+			searchSearchResponse.getAggregationResultsMap();
+
+		TermsAggregationResult nameTermsAggregationResult =
+			(TermsAggregationResult)aggregationResultsMap.get("name");
+
+		for (Bucket bucket : nameTermsAggregationResult.getBuckets()) {
+			TermsAggregationResult versionTermsAggregationResult =
+				(TermsAggregationResult)bucket.getChildAggregationResult(
+					"version");
+
+			Collection<Bucket> versionBuckets =
+				versionTermsAggregationResult.getBuckets();
+
+			if (versionBuckets.size() != 2) {
+				continue;
+			}
+
+			nodeIds.put(
+				_getNodeId(
+					currentProcessVersion, versionTermsAggregationResult),
+				_getNodeId(
+					latestProcessVersion, versionTermsAggregationResult));
+		}
+
+		return nodeIds;
+	}
+
 	private void _transform(long companyId) {
 		if (!_workflowMetricsIndicesAvailabilityChecker.check(companyId)) {
 			return;
@@ -110,7 +242,7 @@ public class WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration
 			Document document = searchHit.getDocument();
 
 			try {
-				_workflowMetricsSLADefinitionTransformer.transform(
+				_transform(
 					document.getLong("companyId"),
 					document.getString("version"),
 					document.getLong("processId"));
@@ -121,8 +253,103 @@ public class WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration
 		}
 	}
 
+	private void _transform(
+			long companyId, String latestProcessVersion, long processId)
+		throws PortalException {
+
+		List<WorkflowMetricsSLADefinition> workflowMetricsSLADefinitions =
+			_workflowMetricsSLADefinitionLocalService.
+				getWorkflowMetricsSLADefinitions(
+					companyId, true, processId, latestProcessVersion,
+					WorkflowConstants.STATUS_APPROVED);
+
+		for (WorkflowMetricsSLADefinition workflowMetricsSLADefinition :
+				workflowMetricsSLADefinitions) {
+
+			_transform(latestProcessVersion, workflowMetricsSLADefinition);
+		}
+	}
+
+	private void _transform(
+			String latestProcessVersion,
+			WorkflowMetricsSLADefinition workflowMetricsSLADefinition)
+		throws PortalException {
+
+		Map<String, String> nodeIdMap = _getNodeIdMap(
+			workflowMetricsSLADefinition.getProcessVersion(),
+			latestProcessVersion, workflowMetricsSLADefinition);
+
+		String[] pauseNodeKeys = _transformNodeKeys(
+			nodeIdMap,
+			StringUtil.split(workflowMetricsSLADefinition.getPauseNodeKeys()));
+		String[] startNodeKeys = _transformNodeKeys(
+			nodeIdMap,
+			StringUtil.split(workflowMetricsSLADefinition.getStartNodeKeys()));
+		String[] stopNodeKeys = _transformNodeKeys(
+			nodeIdMap,
+			StringUtil.split(workflowMetricsSLADefinition.getStopNodeKeys()));
+
+		int status = WorkflowConstants.STATUS_APPROVED;
+
+		if (ArrayUtil.isEmpty(startNodeKeys) ||
+			ArrayUtil.isEmpty(stopNodeKeys)) {
+
+			status = WorkflowConstants.STATUS_DRAFT;
+		}
+
+		_workflowMetricsSLADefinitionLocalService.
+			updateWorkflowMetricsSLADefinition(
+				workflowMetricsSLADefinition.
+					getWorkflowMetricsSLADefinitionId(),
+				workflowMetricsSLADefinition.getCalendarKey(),
+				workflowMetricsSLADefinition.getDescription(),
+				workflowMetricsSLADefinition.getDuration(),
+				workflowMetricsSLADefinition.getName(), pauseNodeKeys,
+				startNodeKeys, stopNodeKeys, status,
+				new ServiceContext() {
+					{
+						setCompanyId(
+							workflowMetricsSLADefinition.getCompanyId());
+						setScopeGroupId(
+							workflowMetricsSLADefinition.getGroupId());
+						setUserId(workflowMetricsSLADefinition.getUserId());
+					}
+				});
+	}
+
+	private String[] _transformNodeKeys(
+		Map<String, String> nodeIdMap, List<String> oldNodeKeys) {
+
+		List<String> newNodeKeys = new ArrayList<>();
+
+		for (String oldNodeKey : oldNodeKeys) {
+			List<String> parts = StringUtil.split(oldNodeKey, CharPool.COLON);
+
+			String oldNodeId = parts.get(0);
+
+			if (!nodeIdMap.containsKey(oldNodeId)) {
+				continue;
+			}
+
+			if (parts.size() == 1) {
+				newNodeKeys.add(nodeIdMap.get(oldNodeId));
+			}
+			else {
+				newNodeKeys.add(
+					StringBundler.concat(
+						nodeIdMap.get(oldNodeId), CharPool.COLON,
+						parts.get(1)));
+			}
+		}
+
+		return newNodeKeys.toArray(new String[0]);
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration.class);
+
+	@Reference
+	private Aggregations _aggregations;
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
@@ -136,6 +363,9 @@ public class WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration
 	@Reference
 	private SearchEngineAdapter _searchEngineAdapter;
 
+	@Reference
+	private SearchRequestExecutor _searchRequestExecutor;
+
 	private TriggerConfiguration _triggerConfiguration;
 
 	@Reference
@@ -143,7 +373,7 @@ public class WorkflowMetricsSLADefinitionTransformerSchedulerJobConfiguration
 		_workflowMetricsIndicesAvailabilityChecker;
 
 	@Reference
-	private WorkflowMetricsSLADefinitionTransformer
-		_workflowMetricsSLADefinitionTransformer;
+	private WorkflowMetricsSLADefinitionLocalService
+		_workflowMetricsSLADefinitionLocalService;
 
 }
