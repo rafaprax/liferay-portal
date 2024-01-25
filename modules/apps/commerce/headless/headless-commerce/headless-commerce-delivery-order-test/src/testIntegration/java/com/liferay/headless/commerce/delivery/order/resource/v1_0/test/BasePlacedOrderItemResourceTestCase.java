fe1fadee18420e8a03832fc5a8da80029da4387c
Joao Victor <joao.victor@liferay.com>
2024-01-25 17:07:46 -0300

    LPD-15513 Change ReflectionTestUtil usage in BasePlacedOrderItemResourceTestCase to directly get the declared fields from classes

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-order-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/order/resource/v1_0/test/BasePlacedOrderItemResourceTestCase.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-order-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/order/resource/v1_0/test/BasePlacedOrderItemResourceTestCase.java
@@ -1104,7 +1104,7 @@ public abstract class BasePlacedOrderItemResourceTestCase {
 		throws Exception {
 
 		return TransformUtil.transform(
-			ReflectionUtil.getDeclaredFields(clazz),
+			clazz.getDeclaredFields(),
 			field -> {
 				if (field.isSynthetic()) {
 					return null;
