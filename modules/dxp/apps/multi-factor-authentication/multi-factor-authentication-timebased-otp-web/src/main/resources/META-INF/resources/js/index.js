0471a7b64406fa74f549775fce75a8c6735d99a6
Christian moura <christian.moura@liferay.com>
2025-02-05 14:38:46 -0300

    LPD-48214 update qr code lib import declaration

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import QRCode from 'qrcode';
+import * as QRCode from 'qrcode';
 
 export function generateQRCode({
 	account,
