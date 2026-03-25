e85fb5eb731de0167a9749513f94091ada4111b6
Lucas Miranda <lucas.miranda@liferay.com>
2026-03-25 10:12:47 -0300

    LPD-82612 Add test

---
+++ modules/dxp/apps/saml/saml-test/src/testIntegration/java/com/liferay/saml/web/internal/portlet/action/test/DeleteSamlSpIdpConnectionMVCActionCommandTest.java
@@ -0,0 +1,133 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saml.web.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.saml.persistence.model.SamlSpIdpConnection;
+import com.liferay.saml.persistence.service.SamlSpIdpConnectionLocalService;
+
+import jakarta.portlet.PortletException;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Lucas Miranda
+ */
+@RunWith(Arquillian.class)
+public class DeleteSamlSpIdpConnectionMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testProcessAction() throws Exception {
+		SamlSpIdpConnection samlSpIdpConnection =
+			_samlSpIdpConnectionLocalService.createSamlSpIdpConnection(0);
+
+		_samlSpIdpConnectionLocalService.updateSamlSpIdpConnection(
+			samlSpIdpConnection);
+
+		Company company = CompanyTestUtil.addCompany();
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+					company.getCompanyId())) {
+
+			User user = UserTestUtil.addCompanyAdminUser(
+				_companyLocalService.getCompanyById(company.getCompanyId()));
+
+			Assert.assertThrows(
+				PortletException.class,
+				() -> _processAction(
+					company, samlSpIdpConnection.getSamlSpIdpConnectionId(),
+					user));
+		}
+		finally {
+			_companyLocalService.deleteCompany(company);
+		}
+
+		User user = UserTestUtil.addCompanyAdminUser(
+			_companyLocalService.getCompanyById(
+				TestPropsValues.getCompanyId()));
+
+		Assert.assertEquals(
+			samlSpIdpConnection,
+			_samlSpIdpConnectionLocalService.fetchSamlSpIdpConnection(
+				samlSpIdpConnection.getSamlSpIdpConnectionId()));
+
+		_processAction(
+			_companyLocalService.getCompanyById(TestPropsValues.getCompanyId()),
+			samlSpIdpConnection.getSamlSpIdpConnectionId(), user);
+
+		Assert.assertNull(
+			_samlSpIdpConnectionLocalService.fetchSamlSpIdpConnection(
+				samlSpIdpConnection.getSamlSpIdpConnectionId()));
+	}
+
+	private void _processAction(
+			Company company, long samlSpIdpConnectionId, User user)
+		throws Exception {
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.addParameter(
+			"samlSpIdpConnectionId", String.valueOf(samlSpIdpConnectionId));
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(company);
+		themeDisplay.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+		themeDisplay.setUser(user);
+
+		mockLiferayPortletActionRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		_mvcActionCommand.processAction(
+			mockLiferayPortletActionRequest,
+			new MockLiferayPortletActionResponse());
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject(
+		filter = "mvc.command.name=/admin/delete_saml_sp_idp_connection",
+		type = MVCActionCommand.class
+	)
+	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private SamlSpIdpConnectionLocalService _samlSpIdpConnectionLocalService;
+
+}
