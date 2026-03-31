ae779f1dd1146e00706c810cd0981a703604616e
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2026-03-31 13:27:58 -0700

    LPD-83784 add type column

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/activator/OAuth2AuthorizationExpandoPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/activator/OAuth2AuthorizationExpandoPortalInstanceLifecycleListener.java
@@ -17,6 +17,7 @@ import com.liferay.oauth2.provider.scope.spi.prefix.handler.PrefixHandlerFactory
 import com.liferay.oauth2.provider.scope.spi.scope.finder.ScopeFinder;
 import com.liferay.oauth2.provider.scope.spi.scope.mapper.ScopeMapper;
 import com.liferay.osb.faro.web.internal.application.ApiApplication;
+import com.liferay.osb.faro.web.internal.controller.api.DemandbaseAccountController;
 import com.liferay.osb.faro.web.internal.controller.api.RecommendationController;
 import com.liferay.osb.faro.web.internal.controller.api.ReportController;
 import com.liferay.petra.function.transform.TransformUtil;
@@ -97,6 +98,7 @@ public class OAuth2AuthorizationExpandoPortalInstanceLifecycleListener
 		}
 
 		addExpandoColumn(expandoTable, "groupId", ExpandoColumnConstants.LONG);
+		addExpandoColumn(expandoTable, "type", ExpandoColumnConstants.STRING);
 	}
 
 	@Override
@@ -113,6 +115,8 @@ public class OAuth2AuthorizationExpandoPortalInstanceLifecycleListener
 
 		_expandoColumnLocalService.deleteColumn(
 			expandoTable.getTableId(), "groupId");
+		_expandoColumnLocalService.deleteColumn(
+			expandoTable.getTableId(), "type");
 
 		List<ExpandoColumn> expandoColumns =
 			_expandoColumnLocalService.getColumns(expandoTable.getTableId());
@@ -186,6 +190,10 @@ public class OAuth2AuthorizationExpandoPortalInstanceLifecycleListener
 	}
 
 	private static final String[][] _SAP_ENTRY_OBJECT_ARRAYS = {
+		{
+			"OAUTH2_" + ApiApplication.OAuth2ScopeAliases.ACCOUNTS_WRITE,
+			DemandbaseAccountController.class.getName() + "#postAccount"
+		},
 		{
 			"OAUTH2_" +
 				ApiApplication.OAuth2ScopeAliases.RECOMMENDATIONS_EVERYTHING,
