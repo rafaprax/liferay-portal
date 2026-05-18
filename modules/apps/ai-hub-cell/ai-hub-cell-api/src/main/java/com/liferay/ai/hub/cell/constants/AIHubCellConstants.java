3b56b6460ec2236f8b318e8426ed99f176b39d11
Rafael Praxedes <rafael.praxedes@liferay.com>
2026-05-18 17:13:01 -0300

    ai-hub-cell-api - drop JWTTokenUtil and add OAuth2 application ERC constant
    Replace the HS256 JWT user-on-behalf-of utility with the AI_HUB_CELL_USER_ON_BEHALF_OF
    external reference code, used by the listener and resource to look up the loopback
    OAuth2 application that mints user tokens via LocalOAuthClient.

---
+++ modules/apps/ai-hub-cell/ai-hub-cell-api/src/main/java/com/liferay/ai/hub/cell/constants/AIHubCellConstants.java
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.cell.constants;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class AIHubCellConstants {
+
+	public static final String OAUTH2_APPLICATION_USER_ON_BEHALF_OF_ERC =
+		"AI_HUB_CELL_USER_ON_BEHALF_OF";
+
+}
